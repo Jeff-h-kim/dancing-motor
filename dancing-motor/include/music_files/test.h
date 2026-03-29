2abad227504fb331d1794304ffcb80f503c858b2
Jeff Kim <64948351+Jeffkim6511@users.noreply.github.com>
2026-03-28 22:40:05 -0700

    completed initial program

---
+++ dancing-motor/include/music_files/test.h
@@ -0,0 +1,26 @@
+#pragma once
+const char *MUSIC_JSON = R"({
+    "title":            "Toothless Dancing",
+    "tempo":            150,
+    "default_duration": 140,
+    "default_gap":      50,
+    "notes": [
+        { "note": "Eb5", "duration": 200, "gap": 20 },
+        { "note": "Bb5", "duration": 200, "gap": 20 },
+        { "note": "A5",  "duration": 200, "gap": 20 },
+        { "note": "Ab5",  "duration": 200, "gap": 20 },
+        { "note": "Gb5",  "duration": 200, "gap": 20 },
+        { "note": "Db5", "duration": 200, "gap": 20 },
+        { "note": "Eb5", "duration": 200, "gap": 20 },
+        { "note": "Db5", "duration": 200, "gap": 20 },
+
+        { "note": "Eb5", "duration": 100, "gap": 120 },
+        { "note": "Eb5", "duration": 100, "gap": 120 },
+        { "note": "Eb5", "duration": 100, "gap": 120 },
+        { "note": "Db5", "duration": 200, "gap": 20 },
+        { "note": "Eb5", "duration": 200, "gap": 20 },
+        { "note": "Bb4", "duration": 200, "gap": 20 },
+        { "note": "Db5", "duration": 200, "gap": 20 },
+        { "note": "Bb4", "duration": 200, "gap": 20 },
+    ]
+})";
