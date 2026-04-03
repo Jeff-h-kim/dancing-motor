e12c4006585034c25619e3d74f061199c9a8195b
Jeff Kim <64948351+Jeffkim6511@users.noreply.github.com>
2026-04-02 21:49:55 -0700

    completed multibuzzer

---
+++ dancing-motor/include/music_files/test.h
@@ -0,0 +1,52 @@
+#pragma once
+
+const char *MELODY_JSON = R"({
+    "title": "Test - Melody",
+    "tempo": 120,
+    "notes": [
+        { "note": "C3",  "duration": 1.0, "type": "reg" },
+         { "note": "C3",  "duration": 1.0, "type": "reg" },
+          { "note": "C3",  "duration": 1.0, "type": "reg" },
+           { "note": "C3",  "duration": 1.0, "type": "reg" },
+
+           { "note": "C3",  "duration": 2.0, "type": "reg" },
+            { "note": "C3",  "duration": 2.0, "type": "reg" },
+
+            { "note": "C3",  "duration": 4.0, "type": "reg" },
+
+    ]
+})";
+
+const char *HARMONY_JSON = R"({
+    "title": "Test - Melody",
+    "tempo": 120,
+    "notes": [
+        { "note": "E3",  "duration": 1.0, "type": "reg" },
+         { "note": "E3",  "duration": 1.0, "type": "reg" },
+          { "note": "E3",  "duration": 1.0, "type": "reg" },
+           { "note": "E3",  "duration": 1.0, "type": "reg" },
+
+           { "note": "E3",  "duration": 2.0, "type": "reg" },
+            { "note": "E3",  "duration": 2.0, "type": "reg" },
+
+            { "note": "E3",  "duration": 4.0, "type": "reg" },
+
+    ]
+})";
+
+const char *BASS_JSON = R"({
+    "title": "Test - Melody",
+    "tempo": 120,
+    "notes": [
+        { "note": "C3",  "duration": 1.0, "type": "reg" },
+         { "note": "C3",  "duration": 1.0, "type": "reg" },
+          { "note": "C3",  "duration": 1.0, "type": "reg" },
+           { "note": "C3",  "duration": 1.0, "type": "reg" },
+
+           { "note": "C3",  "duration": 2.0, "type": "reg" },
+            { "note": "C3",  "duration": 2.0, "type": "reg" },
+
+            { "note": "C3",  "duration": 4.0, "type": "reg" },
+
+    ]
+})";
